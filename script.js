9170d95021d638480ba55d062233ce85ffa04d61
Michal Zielinski <zielinski93.m@gmail.com>
2023-10-16 20:53:53 +0200

    Updated app script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-    const apiRoot = 'http://localhost:8080/v1/tasks';
-    const trelloApiRoot = 'http://localhost:8080/v1/trello';
+    const apiRoot = 'https://task-web-application--michalzielins12.repl.co/v1/';
+    const trelloApiRoot = 'https://task-web-application.michalzielins12.repl.co/v1/trello';
     const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
     const $tasksContainer = $('[data-tasks-container]');
 
